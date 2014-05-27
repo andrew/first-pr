23ed17cca6d121339b5ce25c9d662c23b3370d4d
sturobson <stu@alwaystwisted.com>
2014-05-27 10:33:15 +0100

    adds gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp        = require('gulp');
 var concat      = require('gulp-concat');
+var sass        = require('gulp-sass');
 var minifyCSS   = require('gulp-minify-css');
 var uglify      = require('gulp-uglify');
 
@@ -15,6 +16,12 @@ gulp.task('js', function() {
     .pipe(gulp.dest('.'));
 });
 
+gulp.task('sass', function () {
+  gulp.src('./css/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'));
+});
+
 gulp.task('css', function () {
   gulp.src(paths.styles)
     .pipe(minifyCSS({keepSpecialComments: 0}))
@@ -24,8 +31,9 @@ gulp.task('css', function () {
 
 // Continuous build
 gulp.task('watch', function() {
-  gulp.watch([paths.js],        ['js']);
-  gulp.watch([paths.styles],    ['css']);
+  gulp.watch([paths.js],            ['js']);
+  gulp.watch(['./css/sass/**/*']),  ['sass'];
+  gulp.watch([paths.styles],        ['css']);
 });
 
-gulp.task('default', ['js', 'css', 'watch']);
+gulp.task('default', ['js', 'sass', 'css', 'watch']);
