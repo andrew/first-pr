ad38286394420dcbf81f4336e7662d3abb40c87b
Andrew Nesbitt <andrewnez@gmail.com>
2016-08-02 09:59:57 +0100

    fixed tests

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var test = require('tape');
 
 test('build task completes', function (t) {
   exec('npm run build', (err, stdout, stderr) => {
-    t.assert(!err && !stderr, 'no error')
+    t.assert(!err, 'no error')
     t.assert(/Finished/i.test(stdout), "build")
     t.end()
   })
